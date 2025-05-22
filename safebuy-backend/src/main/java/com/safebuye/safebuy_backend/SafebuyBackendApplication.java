0597c13dbf156c2e50dc7c7e9a7ba2296b0bbcef
Andruortiz <ortiz.andresdom@gmail.com>
2025-05-22 00:03:41 -0500

    back inicializado con spring
    back inicializado con sprint, jdk 21

---
+++ safebuy-backend/src/main/java/com/safebuye/safebuy_backend/SafebuyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.safebuye.safebuy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafebuyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafebuyBackendApplication.class, args);
+	}
+
+}
---
+++ safebuy-backend/src/test/java/com/safebuye/safebuy_backend/SafebuyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safebuye.safebuy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafebuyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
